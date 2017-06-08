e2c8816446cd3cf9247ca1d3b8c4e452e8b10da6
ijancan <ijancan3@gmail.com>
2017-06-08 15:02:36 -0500

    Form template
    Added a form template to the plugin that adds a form at the current line of HTML.

--- main.js
+++ main.js
@@ -4,11 +4,12 @@
 define(function (require, exports, module) {
     "use strict";
     var commandName         = "newHtml";
+	 var newForm = "newForm";
     var Menus               = brackets.getModule("command/Menus"),
         CommandManager      = brackets.getModule("command/CommandManager"),
         EditorManager       = brackets.getModule("editor/EditorManager");
     
-    var template
+    var template, template2;
     /*
      * Implementation
      */
@@ -16,15 +17,24 @@ define(function (require, exports, module) {
         template = require('text!html-template.html');
       EditorManager.getCurrentFullEditor()._codeMirror.setValue(template);
     }
+	function loadform() {
+		template2 = require('text!form-template.html');
+		EditorManager.getCurrentFullEditor()._codeMirror.replaceSelection(template2);
+	}
     /*
      * Command
      */
 CommandManager.register("New html", commandName, loadhtml); 
+CommandManager.register("New form", newForm, loadform); 
     /*
      * Custom menu
      */
 var menu = Menus.addMenu("Html", "edgedocks.custom.menu" );  
- menu.addMenuItem(commandName)
+ menu.addMenuItem(commandName);
+ menu.addMenuItem(newForm);
 });
+/*var menu2 = Menus.addMenu("Form", "edgedocks.custom.menu" );  
+ 
+});*/
 
 
